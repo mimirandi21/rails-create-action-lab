e0621a96105364a8568564fb509e82eefc1e7049
Randi Schreiner <mimirandi21@gmail.com>
2020-11-29 11:05:59 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,22 @@ class StudentsController < ApplicationController
   end
 
   def create
+    
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
   end
+
+  # private
+
+  # def student_params(array_args)
+  #   params.require(:student).permit(array_args)
+  # end
+
+  # def student_array
+  #   [:first_name, :last_name]
+  # end
 
 end
